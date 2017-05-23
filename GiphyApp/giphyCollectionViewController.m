7200949b6ed7a619f4de0d2ed4ffdfec06b825ac
Yuri V <yuriv@michaelmacmini.home>
2017-05-23 21:43:01 +0300

    giphyService update 2

--- GiphyApp/giphyCollectionViewController.m
+++ GiphyApp/giphyCollectionViewController.m
@@ -7,31 +7,32 @@
 //
 
 #import "giphyCollectionCollectionViewController.h"
+#import "gifCell.h"
 
 @interface giphyCollectionCollectionViewController ()
+
+@property (strong, nonatomic) NSArray *gifList;
+
 
 @end
 
 @implementation giphyCollectionCollectionViewController
 
-static NSString * const reuseIdentifier = @"Cell";
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+
+- (void)loadView {
+    [super loadView];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
+    self.gifList = [[NSArray alloc] init];
+    
     
     // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    [self.collectionView registerClass:[gifCell class] forCellWithReuseIdentifier:[gifCell cellIdentifier]];
     
-    // Do any additional setup after loading the view.
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 /*
 #pragma mark - Navigation
@@ -43,28 +44,27 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 */
 
-#pragma mark <UICollectionViewDataSource>
+#pragma mark CollectionView DataSource
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of items
-    return 0;
+
+    return [self.gifList count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:[gifCell cellIdentifier] forIndexPath:indexPath];
     
     // Configure the cell
     
     return cell;
 }
 
-#pragma mark <UICollectionViewDelegate>
+#pragma mark CollectionView Delegate
 
 /*
 // Uncomment this method to specify if the specified item should be highlighted during tracking
